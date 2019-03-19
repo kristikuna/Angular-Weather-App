b8ec5e30fd6df47937f5ccda92b53668536a716d
bertram.m.gage <bertram.m.gage@accenture.com>
2019-03-18 21:50:20 -0500

    updated weather card details to have placeholders for icon

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -21,30 +21,45 @@ export class WeatherComponent implements OnInit {
     id: 1,
     cityName: "Detroit",
     state: "Michigan",
+    stateAB: "MI",
+    weatherIcon: "sample",
+    currentWeather: "weather",
     summary: "Placeholder text"
   }
   event2 = {
     id: 2,
     cityName: "New York City",
     state: "New York",
+    stateAB: "NY",
+    weatherIcon: "sample",
+    currentWeather: "weather",
     summary: "Placeholder text"
   }
   event3 = {
     id: 3,
     cityName: "Chicago",
     state: "Illinois",
+    stateAB: "IL",
+    weatherIcon: "sample",
+    currentWeather: "weather",
     summary: "Placeholder text"
   }
   event4 = {
     id: 4,
     cityName: "San Francisco",
     state: "California",
+    stateAB: "CA",
+    weatherIcon: "sample",
+    currentWeather: "weather",
     summary: "Placeholder text"
   }
   event5 = {
     id: 5,
     cityName: "Austin",
     state: "Texas",
+    stateAB: "TX",
+    weatherIcon: "sample",
+    currentWeather: "weather",
     summary: "Placeholder text"
   }
   constructor() { }
