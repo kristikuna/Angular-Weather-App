dd8aa667ce2d91f1f8cec4f8d695ae64f622b76a
Byrnes, Kristi M <kristi.m.byrnes@accenture.com>
2019-03-19 15:14:57 -0400

    Add ngFor to loop through hard coded city data

--- src/app/weather/weather-container.component.ts
+++ src/app/weather/weather-container.component.ts
@@ -5,51 +5,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-container.component.css']
 })
 export class WeatherComponent implements OnInit {
-  data1 = {
-    id: 1,
-    cityName: "Detroit",
-    state: "Michigan",
-    stateAB: "MI",
-    weatherIcon: "placeholder for icon",
-    currentWeather: "weather",
-    summary: "summary for city"
-  }
-  data2 = {
-    id: 2,
-    cityName: "New York City",
-    state: "New York",
-    stateAB: "NY",
-    weatherIcon: "placeholder for icon",
-    currentWeather: "weather",
-    summary: "summary for city"
-  }
-  data3 = {
-    id: 3,
-    cityName: "Chicago",
-    state: "Illinois",
-    stateAB: "IL",
-    weatherIcon: "placeholder for icon",
-    currentWeather: "weather",
-    summary: "summary for city"
-  }
-  data4 = {
-    id: 4,
-    cityName: "San Francisco",
-    state: "California",
-    stateAB: "CA",
-    weatherIcon: "placeholder for icon",
-    currentWeather: "weather",
-    summary: "summary for city"
-  }
-  data5 = {
-    id: 5,
-    cityName: "Austin",
-    state: "Texas",
-    stateAB: "TX",
-    weatherIcon: "placeholder for icon",
-    currentWeather: "weather",
-    summary: "summary for city"
-  }
+  cities = [
+    {
+      id: 1,
+      cityName: "Detroit",
+      state: "Michigan",
+      stateAB: "MI",
+      weatherIcon: "placeholder for icon",
+      currentWeather: "weather",
+      summary: "summary for city"
+    },
+    {
+      id: 2,
+      cityName: "New York City",
+      state: "New York",
+      stateAB: "NY",
+      weatherIcon: "placeholder for icon",
+      currentWeather: "weather",
+      summary: "summary for city"
+    },
+    {
+      id: 3,
+      cityName: "Chicago",
+      state: "Illinois",
+      stateAB: "IL",
+      weatherIcon: "placeholder for icon",
+      currentWeather: "weather",
+      summary: "summary for city"
+    },
+    {
+      id: 4,
+      cityName: "San Francisco",
+      state: "California",
+      stateAB: "CA",
+      weatherIcon: "placeholder for icon",
+      currentWeather: "weather",
+      summary: "summary for city"
+    },
+    {
+      id: 5,
+      cityName: "Austin",
+      state: "Texas",
+      stateAB: "TX",
+      weatherIcon: "placeholder for icon",
+      currentWeather: "weather",
+      summary: "summary for city"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
