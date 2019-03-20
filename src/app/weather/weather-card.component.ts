efb972541514841fba8f592c43699d683b3120c3
Byrnes, Kristi M <kristi.m.byrnes@accenture.com>
2019-03-20 11:31:50 -0400

    Give card component a template URL

--- src/app/weather/weather-card.component.ts
+++ src/app/weather/weather-card.component.ts
@@ -2,25 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'tv-weather-card',
-  template: `
-   <div class="card">
-    <div class="card-header">
-      <h5>{{data.name}}</h5>
-    </div>
-    <div class="card-content">
-      <br />{{data.main.temp}} &#176;
-      <br />placeholder for temp in cesius
-      <br />{{data.weather.description}}
-      <div [ngSwitch]="data.weather.description">
-        <img *ngSwitchCase="'overcast clouds'" src="assets/cloudy.png" alt="">
-        <img *ngSwitchCase="'sunny'" src="assets/sunny.png" alt="">
-        <img *ngSwitchCase="'snow'" src="assets/snow.png" alt="">
-        <img *ngSwitchCase="'rainy'" src="assets/rain.png" alt="">
-        <img *ngSwitchCase="'partly cloudy'" src="assets/partly_cloudy.png" alt="">
-      </div>
-    </div>
-   </div>
-  `,
+  templateUrl: './weather-card.component.html',
   styleUrls: ['./weather-container.component.css']
 })
 export class WeatherCardComponent {
